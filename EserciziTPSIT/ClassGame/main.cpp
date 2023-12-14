9f691684ad74a61da9a2b1e0fed9665ecc3c6355
Richie <9698113@itisrossi.vi.it>
2023-12-14 13:16:01 +0100

    inizio

--- EserciziTPSIT/ClassGame/main.cpp
+++ EserciziTPSIT/ClassGame/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <string.h>
+using namespace std;
+
+
+class Game {
+
+private:
+    char* parola;
+    string nome;
+    int vite = 10;
+    int len;
+    string parolaCifrata;
+
+public:
+
+    //costruttore
+    Game();
+    Game(const char *s = nullptr, string Nome = "Nessuno"){
+        parola = new char[strlen(s) + 1];
+        strcpy(parola, s);
+        len = strlen(parola);
+        nome = Nome;
+        cout << "Benvenuto " << nome << endl;
+    }
+
+    string cifraParola()
+    {
+        for (int i=0; i<len; ++i)
+        {
+            parolaCifrata += "_";
+        }
+
+        return parolaCifrata;
+    }
+
+    string controlloCarattere(char* c = "r")
+    {
+        for (int i=0; i<len; ++i)
+        {
+            if(parola[i] == *c)
+            {
+                parolaCifrata[i] == *c;
+            }
+        }
+        return parolaCifrata;
+    }
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Game gioco("Riccardo", "Meggio");
+    cout << gioco.cifraParola() << endl;
 }
